8685a9ca77bc27ec3edafd072219517b6a5f0ea1
kabiruH <kelkab46@gmail.com>
2024-06-13 14:37:09 +0300

    added signin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,90 +1,101 @@
-import React from 'react';
+import React,{useState} from 'react'
+import {Facebook,GitHub,Google} from '@mui/icons-material'
 
+const SignIn = () => {
 
-export default function SignIn() {
-  return (
-    <div className='main-container w-[1920px] h-[1152px] bg-[#fff] relative overflow-hidden mx-auto my-0'>
-      <div className='w-[965px] h-[1152px] absolute top-0 left-[960px] z-[34]'>
-        <div className='w-[190px] h-[190px]  bg-cover bg-no-repeat rounded-[50%] relative z-[45] mt-[348px] mr-0 mb-0 ml-[371px]' />
-        <span className="flex w-[301px] h-[46px] justify-center items-start font-['Plus_Jakarta_Sans'] text-[32px] font-extrabold leading-[45.415px] text-[#000] relative text-center whitespace-nowrap z-[36] mt-[15px] mr-0 mb-0 ml-[331px]">
-          Organization Name
-        </span>
-        <span className="flex w-[580px] h-[46px] justify-center items-start font-['Plus_Jakarta_Sans'] text-[40px] font-bold leading-[45.415px] text-[#000] tracking-[2px] relative text-center whitespace-nowrap z-[35] mt-[60px] mr-0 mb-0 ml-[192px]">
-          Project Management Portal
-        </span>
-        <div className='w-[622px] h-[60px] relative z-[43] mt-[357px] mr-0 mb-0 ml-[308px]'>
-          <div className='w-[60px] h-[60px]  bg-cover bg-no-repeat rounded-[50%] absolute top-0 left-[562px] z-[42]' />
-          <div className='w-[18px] h-[18px] absolute top-[21px] left-[583px] overflow-hidden z-[43]'>
-            <div className='w-[16.087px] h-[18px]  bg-[length:100%_100%] bg-no-repeat relative z-[44] mt-0 mr-0 mb-0 ml-[0.96px]' />
+  const [isLogin,setIsLogin] = useState(true);
+
+  const [loginEmail,setLoginEmail] = useState("");
+  const [loginPassword,setLoginPassword] = useState("");
+
+  const [registerEmail,setRegisterEmail] = useState("");
+  const [registerPassword,setRegisterPassword] = useState("");
+  const [registerName,setRegisterName] = useState("");
+  const [registerAvatar,setRegisterAvatar] = useState("");
+  
+  const LoginForm = () => {
+    return(
+       <div className="bg-white rounded-2xl shadow-2xl flex flex-col w-full md:w-1/3 items-center max-w-4xl transition duration-1000 ease-out">
+             <h2 className='p-3 text-3xl font-bold text-pink-400'>Horiz</h2>
+             <div className="inline-block border-[1px] justify-center w-20 border-blue-400 border-solid"></div>
+             <h3 className='text-xl font-semibold text-blue-400 pt-2'>Sign In!</h3>
+             <div className='flex space-x-2 m-4 items-center justify-center'>
+                <div className="socialIcon">
+                <Facebook/>
+                </div>
+                <div className="socialIcon">
+                <GitHub/>
+                </div>
+                <div className="socialIcon">
+                <Google/>  
+                </div>
+             </div>
+             {/* Inputs */}
+             <div className='flex flex-col items-center justify-center'>
+              <input type='email' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Email'></input>
+              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Password'></input>
+              <button className='rounded-2xl m-2 text-white bg-blue-400 w-2/5 px-4 py-2 shadow-md hover:text-blue-400 hover:bg-white transition duration-200 ease-in'>
+                Sign In
+              </button>
+             </div>
+             <div className="inline-block border-[1px] justify-center w-20 border-blue-400 border-solid"></div>
+             <p className='text-blue-400 mt-4 text-sm'>Don't have an account?</p>
+             <p className='text-blue-400 mb-4 text-sm font-medium cursor-pointer' onClick={() => setIsLogin(false)}>Create a New Account?</p>
           </div>
-          <button className="w-[88px] h-[24px] font-['DM_Sans'] text-[14px] font-medium leading-[24px] text-[#000] border-none tracking-[-0.28px] absolute top-1/2 left-0 translate-x-0 translate-y-[-16.67%] whitespace-nowrap z-[39] pointer" />
-          <button className="w-[106px] h-[24px] font-['DM_Sans'] text-[14px] font-medium leading-[24px] text-[#000] border-none tracking-[-0.28px] absolute top-1/2 left-[186px] translate-x-0 translate-y-[-16.67%] whitespace-nowrap z-40 pointer" />
-        </div>
+    )
+  }
+  
+  const  SignUpForm = () => {
+     return(
+        <div className="bg-blue-400 text-white rounded-2xl shadow-2xl  flex flex-col w-full  md:w-1/3 items-center max-w-4xl transition duration-1000 ease-in">
+              <h2 className='p-3 text-3xl font-bold text-white'>Horiz</h2>
+             <div className="inline-block border-[1px] justify-center w-20 border-white border-solid"></div>
+             <h3 className='text-xl font-semibold text-white pt-2'>Create Account!</h3>
+             <div className='flex space-x-2 m-4 items-center justify-center'>
+                <div className="socialIcon border-white">
+                <Facebook className="text-white"/>
+                </div>
+                <div className="socialIcon border-white">
+                <GitHub className="text-white"/>
+                </div>
+                <div className="socialIcon border-white">
+                <Google className="text-white"/>  
+                </div>
+             </div>
+             {/* Inputs */}
+             <div className='flex flex-col items-center justify-center mt-2'>
+             <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Name'></input>
+              <input type='email' className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Email'></input>
+              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Password'></input>
+              <input type="password" className='rounded-2xl px-2 py-1 w-4/5 md:w-full border-[1px] border-blue-400 m-1 focus:shadow-md focus:border-pink-400 focus:outline-none focus:ring-0' placeholder='Avatar URL'></input>
+              <button className='rounded-2xl m-4 text-blue-400 bg-white w-3/5 px-4 py-2 shadow-md hover:text-white hover:bg-blue-400 transition duration-200 ease-in'>
+                Sign Up
+              </button>
+             </div>
+             <div className="inline-block border-[1px] justify-center w-20 border-white border-solid"></div>
+             <p className='text-white mt-4 text-sm'>Already have an account?</p>
+             <p className='text-white mb-4 text-sm font-medium cursor-pointer' onClick={() => setIsLogin(true)}>Sign In to your Account?</p>
+          </div>
+     )
+  }
+
+  return (
+    <div className="bg-gray-100 flex flex-col items-center justify-center min-h-screen md:py-2">
+    <main className="flex items-center w-full px-2 md:px-20">
+      <div className="hidden md:inline-flex flex-col flex-1 space-y-1">
+        <p className='text-6xl text-blue-500 font-bold'>Horiz</p>
+        <p className='font-medium text-lg leading-1 text-pink-400'>Explore your interests, meet new friends & expand your horions</p>
       </div>
-      <span className="flex h-[56px] justify-start items-start font-['DM_Sans'] text-[36px] font-bold leading-[56px] text-[#000] tracking-[-0.72px] absolute top-[271px] left-[308px] text-left whitespace-nowrap z-[32]">
-        Sign In
-      </span>
-      <span className="flex h-[16px] justify-start items-start font-['DM_Sans'] text-[16px] font-normal leading-[16px] text-[#000] tracking-[-0.32px] absolute top-[335px] left-[309px] text-left whitespace-nowrap z-[33]">
-        Enter your username and password to sign in!
-      </span>
-      <div className="w-[72px] h-[14px] font-['DM_Sans'] text-[14px] font-medium leading-[14px] tracking-[-0.28px] absolute top-[411px] left-[309px] text-left whitespace-nowrap z-[24]">
-        <span className="font-['DM_Sans'] text-[14px] font-medium leading-[14px] text-[#000] tracking-[-0.28px] relative text-left">
-          Username
-        </span>
-        <span className="font-['DM_Sans'] text-[14px] font-medium leading-[14px] text-[#000] tracking-[-0.28px] relative text-left">
-          *
-        </span>
-      </div>
-      <div className='w-[21.35%] h-[4.34%] border-solid border border-[#000] absolute top-[38.02%] left-[16.09%] z-[27]'>
-        <input className='w-[410px] h-[50px] bg-transparent border-none absolute top-[-1px] left-[-1px] z-30' />
-      </div>
-      <div className="w-[68px] h-[14px] font-['DM_Sans'] text-[14px] font-medium leading-[14px] tracking-[-0.28px] absolute top-[512px] left-[309px] text-left whitespace-nowrap z-[15]">
-        <span className="font-['DM_Sans'] text-[14px] font-medium leading-[14px] text-[#000] tracking-[-0.28px] relative text-left">
-          Password
-        </span>
-        <span className="font-['DM_Sans'] text-[14px] font-medium leading-[14px] text-[#000] tracking-[-0.28px] relative text-left">
-          *
-        </span>
-      </div>
-      <div className='w-[21.35%] h-[4.34%] border-solid border border-[#000] absolute top-[46.79%] left-[16.09%] z-[18]'>
-        <input className='w-[410px] h-[50px] bg-transparent border-none absolute top-[-1px] left-[-1px] z-[21]' />
-      </div>
-      <span className="flex h-[20px] justify-start items-start font-['DM_Sans'] text-[14px] font-normal leading-[20px] text-[#000] tracking-[-0.28px] absolute bottom-[512px] left-[338px] text-left whitespace-nowrap z-[8]">
-        Keep me logged in
-      </span>
-      <span className="flex h-[20px] justify-start items-start font-['DM_Sans'] text-[14px] font-medium leading-[20px] text-[#000] tracking-[-0.28px] absolute top-[620px] left-[589px] text-left whitespace-nowrap z-[13]">
-        Forget password?
-      </span>
-      <div className='w-[18px] h-[18px] bg-[#000] absolute top-[622px] left-[309px] z-[9]'>
-        <div className='w-[16px] h-[16px] relative overflow-hidden z-10 mt-px mr-0 mb-0 ml-px'>
-          <div className='w-[11.314px] h-[8.014px] bg-[length:100%_100%] bg-no-repeat relative z-[11] mt-[3.986px] mr-0 mb-0 ml-[2.424px]' />
-        </div>
-      </div>
-      <div className='flex w-[410px] h-[54px] pt-[10px] pr-[8px] pb-[10px] pl-[8px] gap-[10px] justify-center items-center flex-nowrap bg-[#000] absolute top-[673px] left-[309px] z-[4]'>
-        <span className="flex w-[43px] h-[14px] justify-center items-start shrink-0 basis-auto font-['DM_Sans'] text-[14px] font-bold leading-[14px] text-[#fff] tracking-[-0.28px] relative text-center whitespace-nowrap z-[5]">
-          Sign In
-        </span>
-      </div>
-      <div className="w-[244px] h-[26px] font-['DM_Sans'] text-[14px] font-bold leading-[26px] tracking-[-0.28px] absolute top-[753px] left-[309px] text-left whitespace-nowrap z-[2]">
-        <span className="font-['DM_Sans'] text-[14px] font-normal leading-[26px] text-[#000] tracking-[-0.28px] relative text-left">
-          Not registered yet?
-        </span>
-        <span className="font-['DM_Sans'] text-[14px] font-bold leading-[26px] text-[#707eae] tracking-[-0.28px] relative text-left">
-          
-        </span>
-        <span className="font-['DM_Sans'] text-[14px] font-bold leading-[26px] text-[#000] tracking-[-0.28px] relative text-left">
-          Create
-        </span>
-        <span className="font-['DM_Sans'] text-[14px] font-bold leading-[26px] text-[#4218ff] tracking-[-0.28px] relative text-left">
-          
-        </span>
-        <span className="font-['DM_Sans'] text-[14px] font-bold leading-[26px] text-[#000] tracking-[-0.28px] relative text-left">
-          an Account
-        </span>
-      </div>
-      <span className="flex h-[24px] justify-start items-start font-['DM_Sans'] text-[14px] font-medium leading-[24px] text-[#000] tracking-[-0.28px] absolute top-[calc(50%--512px)] left-[309px] text-left whitespace-nowrap z-[38]">
-        © 2024 Optimum Computer Systems. All Rights Reserved.
-      </span>
+      {
+        isLogin? (
+         <LoginForm/>
+        ):(
+         <SignUpForm/>
+        )
+      }
+    </main>
     </div>
-  );
+  )
 }
+
+export default SignIn

11e0996430cbc70d7f55e47ae11812e1dff34216
KabiruH <kelkab46@gmail.com>
2024-07-23 15:11:31 +0300

    to fix counties

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://192.168.100.5:5248',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://192.168.100.10:5248',
   headers: {
     'Content-Type': 'application/json',
   },
